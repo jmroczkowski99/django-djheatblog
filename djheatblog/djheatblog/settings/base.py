7ac45235979638639a0f851be6607ce957d1e082
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-11-18 00:45:28 +0100

    Start app, requirements fix

--- djheatblog/djheatblog/settings/base.py
+++ djheatblog/djheatblog/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djheatblog.blog',
 ]
 
 MIDDLEWARE = [
@@ -97,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
