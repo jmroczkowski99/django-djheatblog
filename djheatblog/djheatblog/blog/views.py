c602731f293982bef9990d3d6ee0df26eda6bb0b
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-11-26 01:30:54 +0100

    feat: add basic post info for each post

--- djheatblog/djheatblog/blog/views.py
+++ djheatblog/djheatblog/blog/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 
 from .models import Post
 
+
 class HomeView(ListView):
     model = Post
     template_name = "blog/index.html"
+    context_object_name = "posts"
