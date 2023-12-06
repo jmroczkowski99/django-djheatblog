bc31ae2e58de83036daaeab8737e1295aeb162bc
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-06 20:55:38 +0100

    refactor: move latest post function to HomeView

--- djheatblog/djheatblog/blog/views.py
+++ djheatblog/djheatblog/blog/views.py
@@ -15,8 +15,17 @@ class HomeView(ListView):
             return "blog/components/post-list-elements.html"
         return "blog/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        latest = self.get_latest_post()
+        context['latest'] = latest
+        return context
+
+    def get_latest_post(self):
+        return Post.objects.latest("updated_at")
+
+
 def post_single(request, post):
     post = get_object_or_404(Post, slug=post, status="published")
     related = Post.objects.filter(author=post.author)[:5]
-    latest = Post.objects.latest("updated_at")
-    return render(request, "blog/single.html", {"post": post, "related": related, "latest": latest})
+    return render(request, "blog/single.html", {"post": post, "related": related})
