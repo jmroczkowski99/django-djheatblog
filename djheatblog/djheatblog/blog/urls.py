62779bc5f55db5948031b4091d2b3203d39db6e8
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-09 18:32:16 +0100

    fix: change hierarchy of urls

--- djheatblog/djheatblog/blog/urls.py
+++ djheatblog/djheatblog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="homepage"),
-    path("<slug:post>/", views.post_single, name="post_single"),
     path("tag/<tag>/", views.tags, name="tag"),
     path("search/", views.SearchResultsView.as_view(), name="search_results"),
+    path("<slug:post>/", views.post_single, name="post_single"),
 ]
