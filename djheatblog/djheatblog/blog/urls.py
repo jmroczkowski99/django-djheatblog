8555d2c0835b717061a76c46f9919a3ca559e143
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-09 18:11:47 +0100

    feat: add search_results url

--- djheatblog/djheatblog/blog/urls.py
+++ djheatblog/djheatblog/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="homepage"),
     path("<slug:post>/", views.post_single, name="post_single"),
     path("tag/<tag>/", views.tag, name="tag"),
+    path("search/", views.SearchResultsView.as_view(), name="search_results"),
 ]
